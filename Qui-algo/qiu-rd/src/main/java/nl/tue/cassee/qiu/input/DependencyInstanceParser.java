961433b59395c08213061b29083a71739ad2efc0
Nathan Cassee <n.w.cassee@tue.nl>
2019-11-06 10:18:14 +0100

    Update to handle oddities of the Stanford API.

--- Qui-algo/qiu-rd/src/main/java/nl/tue/cassee/qiu/input/DependencyInstanceParser.java
+++ Qui-algo/qiu-rd/src/main/java/nl/tue/cassee/qiu/input/DependencyInstanceParser.java
@@ -8,8 +8,8 @@ import java.util.List;
  * Given a list of strings in a dependency instance format
  * parses these to extract DependencyInstances.
  * 
- * Format should be '<RELNAME>(<SWORD>/<STAG>-<TWORD>/<TTAG>)'
- * where consecutive tags are seperated by semi-colons. 
+ * Format should be '<RELNAME>(<TWORD>/<TTAG>-<SWORD>/<STAG>)'
+ * where consecutive tags are seperated by semicolons. 
  * 
  * SWORD = Source word,
  * STAG = Source tag,
@@ -28,20 +28,20 @@ public class DependencyInstanceParser {
         for (var dep : depString.split(";")) {
             var relName = dep.split("\\(")[0];
 
-            var sourceRaw = dep.split("\\(")[1].split("\\-")[0];
-
-            var sourceWord = sourceRaw.split("\\/")[0];
-            var sourceTag = sourceRaw.split("\\/")[1];
-
-            
-            var targetRaw = dep.split("\\(")[1].split("\\-")[1];
+            var targetRaw = dep.split("\\(")[1].split("\\-")[0];
 
             var targetWord = targetRaw.split("\\/")[0];
-            var targetTag = targetRaw.split("\\/")[1].replace(")", "");
+            var targetTag = targetRaw.split("\\/")[1];
 
-            output.add(new DependencyInstance(relName.trim(),  
-                            new DependencyInstance.Node(targetWord.trim(), targetTag.trim()),
-                            new DependencyInstance.Node(sourceWord.trim(), sourceTag.trim())));
+            
+            var sourceRaw = dep.split("\\(")[1].split("\\-")[1];
+
+            var sourceWord = sourceRaw.split("\\/")[0];
+            var sourceTag = sourceRaw.split("\\/")[1].replace(")", "");
+
+            output.add(new DependencyInstance(relName.trim(), 
+                            new DependencyInstance.Node(sourceWord.trim(), sourceTag.trim()), 
+                            new DependencyInstance.Node(targetWord.trim(), targetTag.trim())));
         }
 
         return output;
