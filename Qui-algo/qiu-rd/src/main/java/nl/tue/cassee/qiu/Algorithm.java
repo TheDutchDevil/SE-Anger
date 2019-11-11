6b83b72d7be975152ab1dcf031968dfa342c2176
Nathan Cassee <n.w.cassee@tue.nl>
2019-11-11 11:52:44 +0100

    Initial implementation of fixed point algorithm

--- Qui-algo/qiu-rd/src/main/java/nl/tue/cassee/qiu/Algorithm.java
+++ Qui-algo/qiu-rd/src/main/java/nl/tue/cassee/qiu/Algorithm.java
@@ -2,11 +2,16 @@ package nl.tue.cassee.qiu;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import nl.tue.cassee.qiu.input.ParsedSentence;
+import nl.tue.cassee.qiu.rules.Rule1;
+import nl.tue.cassee.qiu.rules.Rule2;
+import nl.tue.cassee.qiu.rules.Rule3;
+import nl.tue.cassee.qiu.rules.Rule4;
 
 /**
  * Implementation of the algorithm as proposed by Qiu et al in Opinion Word
@@ -26,12 +31,49 @@ public class Algorithm {
 
         Logger.atInfo().log("Starting Qiu algorithm with {} seeds and {} sentences", seedList.size(), parsedSentences.size());
 
-        List<String> seedsDiscovered = new ArrayList<>();
+        List<String> opinionsDiscovered = new ArrayList<>(seedList);
         List<String> featuresDiscovered = new ArrayList<>();
 
-        for(var sent : parsedSentences ) {
-            Logger.atTrace().log("Processing sentence '{}'", sent.getSentence());
-        }
+        var newlyFound = 0;
+
+        do {
+            List<String> newFeatures = new ArrayList<>();                
+            List<String> newOpinions = new ArrayList<>();
+
+            newlyFound = 0;
+
+            for(var sent : parsedSentences ) {
+                Logger.atTrace().log("Processing sentence '{}'", sent.getSentence());
+
+                newFeatures.addAll(Rule1.process1(opinionsDiscovered, sent));
+                newFeatures.addAll(Rule1.process2(opinionsDiscovered, sent));
+                newFeatures.addAll(Rule3.process1(opinionsDiscovered, sent));
+                newFeatures.addAll(Rule3.process2(opinionsDiscovered, sent));
+
+                newFeatures = newFeatures.stream().distinct().collect(Collectors.toList());
+
+                newOpinions.addAll(Rule2.process1(featuresDiscovered, sent));
+                newOpinions.addAll(Rule2.process2(featuresDiscovered, sent));
+                newOpinions.addAll(Rule4.process1(featuresDiscovered, sent));
+                newOpinions.addAll(Rule4.process2(featuresDiscovered, sent));
+
+                newOpinions = newOpinions.stream().distinct().collect(Collectors.toList());
+            }
+
+            for(var feat : newFeatures) {
+                if(!featuresDiscovered.contains(feat)) {
+                    newlyFound++;
+                    featuresDiscovered.add(feat);
+                }
+            }
+
+            for(var opinion : newOpinions) {
+                if(!opinionsDiscovered.contains(opinion)) {
+                    newlyFound++;
+                    opinionsDiscovered.add(opinion);
+                }
+            }
+        } while(newlyFound > 0);
 
         return new Output();
     }
