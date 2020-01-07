1b5df9a5617e4c136b4ca644b70c6e51722e0ce7
Nathan Cassee <n.w.cassee@tue.nl>
2020-01-07 13:51:35 +0100

    Refactor build pipeline so self supporting jars can be built.

--- StanfordNlpProcessor/stanfordNlp-processor/src/main/java/main/App.java
+++ StanfordNlpProcessor/stanfordNlp-processor/src/main/java/main/App.java
@@ -1,17 +1,5 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.stream.Collectors;
-
-import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.EnhancedPlusPlusDependenciesAnnotation;
-import edu.stanford.nlp.util.CoreMap;
 import main.processing.PreProcessing;
 import picocli.CommandLine;
 
